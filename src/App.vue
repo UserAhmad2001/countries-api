88b27563909ce899fefe74fc87971673bbe3336f
Ahmad <Kingahmed19991@gmail.com>
2022-08-30 18:58:48 +0300

    completed setting up the webdites dom

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,53 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {getAllCountries, getCountryByName} from './demo'
+</script>
+<script>
+  export default {
+    data(){
+      return {
+        nightMode:true,
+        countries:[]
+      }
+    },
+    beforeMount(){
+      this.countries = getAllCountries()
+      console.log(countries);
+    }
+  }
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <header>
+    <h1>Where in the world?</h1>
+    <div class="theme-btn">
+      <i></i>
+      <h1>{{ nightMode ? 'Dark' : 'Light' }} Mode</h1>
+    </div>
+  </header>
+
+  <section class="filter-sec">
+    <div class="in">
+      <i></i>
+      <input placeholder="Search for a country...">
+    </div>
+
+    <select id="regions-filter">
+      <option value="Africa">Africa</option>
+      <option value="America">America</option>
+      <option value="Asia">Asia</option>
+      <option value="Europe">Europe</option>
+      <option value="Oceania">Oceania</option>
+    </select>
+  </section>
+
+
+  <div class="countries">
+    <country v-for="item in countries" />
+  </div>
+
 </template>
 
 <style scoped lang="scss">
 </style>
+
+
